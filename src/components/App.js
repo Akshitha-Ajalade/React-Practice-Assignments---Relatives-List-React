d38872f7118c2e337b7830f50474b817d0ca182d
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-08 17:43:13 +0530

    updated key

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,15 @@ import "../styles/App.css";
 
 class App extends Component {
   relativeList = [
-    { keyId: "relativeListItem1", relativeList: "idk1" },
-    { keyId: "relativeListItem2", relativeList: "idk2" },
-    { keyId: "relativeListItem3", relativeList: "idk3" }
+    { keyId: "relativeListItem1", relativeList: "rName1" },
+    { keyId: "relativeListItem2", relativeList: "rName2" },
+    { keyId: "relativeListItem3", relativeList: "rName3" }
   ];
 
   render() {
     return (
       <div id="main">
-        <ol>
+        <ol key="olistKey">
           {this.relativeList.map((e) => (
             <li key={e.keyId}> {e.relativeList} </li>
           ))}
