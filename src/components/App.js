dae4a1010a5af08a9429ba3eb9fc0062c34423e3
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-08 14:46:04 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  relativeList = [
+    { keyId: "relativeListItem1", relativeList: "idk1" },
+    { keyId: "relativeListItem2", relativeList: "idk2" },
+    { keyId: "relativeListItem3", relativeList: "idk3" }
+  ];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol>
+          {this.relativeList.map((e) => (
+            <li key={e.keyId}> {e.relativeList} </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
